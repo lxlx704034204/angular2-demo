2460fbf3c74a7a6be86f4c38027fe08ae15f6856
lizhonghui <lzhspace@gmail.com>
2017-03-20 22:40:33 +0800

    docs(): 完善core-js注释

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
-import 'reflect-metadata';
 import 'zone.js';
+
+// 如果希望你的应用支持更多浏览器，可以反注释下面这样，
+// import core-js;
+
+// JiT模式下 reflect-metadata 必不可少。
+// core-js 包含 reflect-metadata，如果引入了 core-js，需注释下面 reflect-metadata 的导入。
+import 'reflect-metadata';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
