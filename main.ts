0e6a551fd930cd3d9153191d90b905f4452f7cbb
lizhonghui <lzhspace@gmail.com>
2017-04-06 16:18:45 +0800

    fix: add missing quotes

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import 'zone.js';
 
 // 如果希望你的应用支持更多浏览器，可以反注释下面这样，
-// import core-js;
+// import 'core-js';
 
 // JiT模式下 reflect-metadata 必不可少。
 // core-js 包含 reflect-metadata，如果引入了 core-js，需注释下面 reflect-metadata 的导入。
