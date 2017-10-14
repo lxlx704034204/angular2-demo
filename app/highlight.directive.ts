5b05f96149eeb9bf492d20bd7a93ae4e4d14df6f
lxlx704034204 <704034204@qq.com>
2017-10-14 21:44:50 +0800

    / 自定义的 高亮 指令

--- app/highlight.directive.ts
+++ app/highlight.directive.ts
@@ -3,11 +3,12 @@ import { Directive, ElementRef, Renderer } from '@angular/core';
 @Directive({
   selector: "[highlight]"
 })
-export class HighlightDirective {
+export class HighlightDirective { // 自定义的 高亮 指令
   constructor(
-    private el: ElementRef, 
-    private renderer: Renderer
+    private el: ElementRef,    //元素
+    private renderer: Renderer //渲染器
   ) { 
     renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'yellow');
+      //                        获取元素本地引用
   }
 }
