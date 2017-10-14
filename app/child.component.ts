67bd1a3fb06674efcf3b8477c10a3c9595f3739b
lxlx704034204 <704034204@qq.com>
2017-10-14 22:11:47 +0800

    //父子组件通讯之 子组件

--- app/child.component.ts
+++ app/child.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core'; 
 
 @Component({
   selector: 'my-child',
   templateUrl: './app/child.component.html'
 })
-export class ChildComponent {
-  @Input() private message: string;
-  @Output() private outer = new EventEmitter<string>();
+export class ChildComponent { //父子组件通讯之 子组件
+  @Input() private message: string; 			//子组件 想要接收父组件的msg，必须定义输入属性@Input()
+  @Output() private outer = new EventEmitter<string>(); //子组件 想要send msg就必须 定义输出属性    @Output()(通常与EventEmitter配合使用)
 	constructor() {	}
   
   sendToParent() {
