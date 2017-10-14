b5e0457ba5e0bdee1c4be4ba966fbf3da1f56e3b
lxlx704034204 <704034204@qq.com>
2017-10-14 21:21:30 +0800

    an2入口typescript文件

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
-import 'zone.js';
+//import 'reflect-metadata';//这个库处理元数据相关的
+import 'zone.js';//catch浏览器的一些异步事件，帮助an2实现高效的变化检测特性
 
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+
 
 // JiT启动模式
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+//  .then(success => console.log('bootstrap success'))
+//  .catch(error => console.log(error))
+;
