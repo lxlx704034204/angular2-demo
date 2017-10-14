683244b70d6c61e687b01a56dc34650c7bd5820a
lxlx704034204 <704034204@qq.com>
2017-10-14 21:47:21 +0800

    1// 为项目 导入 自定义的 高亮 指令

--- app/highlight.directive.ts
+++ app/highlight.directive.ts
@@ -3,7 +3,7 @@ import { Directive, ElementRef, Renderer } from '@angular/core';
 @Directive({
   selector: "[highlight]"
 })
-export class HighlightDirective { // 自定义的 高亮 指令
+export class HighlightDirective { // 自定义的 高亮 指令。项目要使用此指令还必须在app.module.ts根模块中导入进去
   constructor(
     private el: ElementRef,    //元素
     private renderer: Renderer //渲染器
